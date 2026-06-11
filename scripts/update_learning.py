e5e93f0dbca422f4deabd1845f55eeea9e22f358
ak47kavan <124173523+ak47kavan@users.noreply.github.com>
2026-06-11 14:22:59 +0530

    upgrade developer os v2 error fix

--- scripts/update_learning.py
+++ scripts/update_learning.py
@@ -6,7 +6,7 @@ with open("data/learning.json", "r") as f:
 
 today = str(date.today())
 
-log = f"{today} | Arch:{data['arch_linux']} | DSA:{data['dsa']} | Qt6:{data['qt6']} | OS:{data['od_dev']}\n "
+log = f"{today} | Arch:{data['arch_linux']} | DSA:{data['dsa']} | Qt6:{data['qt6']} | OS:{data['os_dev']}\n "
 
 with open("data/history.log","a") as f:
     f.write(log)
